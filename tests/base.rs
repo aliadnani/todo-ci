e7ecbfda29a508a1f8b1d8fe3e0067837388ade4
Ali Adnan <ali.adnani@hotmail.com>
2022-07-09 18:48:11 +0800

    Added baseline tests

--- tests/base.rs
+++ tests/base.rs
@@ -1,12 +1,48 @@
-// use std::path::{Path, PathBuf};
+use std::path::Path;
 
-// #[test]
-// fn test_add() {
-//     let search_results = todo_ci::core::search(
-//         Path::new("./resources/base").to_path_buf(),
-//         false,
-//         "*".to_string(),
-//     )
-//     .unwrap();
-//     // assert_eq!(display::search(3, 2), 5);
-// }
+use chrono::NaiveDate;
+
+#[test]
+fn baseline_test() {
+    let search_results = todo_ci::core::search(
+        Path::new("./tests/resources/base").to_path_buf(),
+        false,
+        "*".to_string(),
+    )
+    .unwrap();
+
+    assert_eq!(search_results.files_searched, 3);
+
+    // TODO: find a way to mock time
+    // Overude TODO on line 3
+    assert_eq!(search_results.overdue_todos.len(), 1);
+    assert_eq!(
+        search_results.overdue_todos[0].date,
+        NaiveDate::from_ymd(1991, 7, 10)
+    );
+    assert_eq!(
+        search_results.overdue_todos[0].description,
+        "Print something besides \"Hello World!\""
+    );
+    assert_eq!(search_results.overdue_todos[0].line_number, 3);
+    assert_eq!(
+        search_results.valid_todos[0].file,
+        Path::new("./tests/resources/base/file_with_todos.rs").to_path_buf()
+    );
+
+    // Valid TODO on line 4
+    assert_eq!(search_results.valid_todos.len(), 1);
+    assert_eq!(
+        search_results.valid_todos[0].date,
+        NaiveDate::from_ymd(2221, 7, 10)
+    );
+    assert_eq!(
+        search_results.valid_todos[0].description,
+        "Print something besides \"Hello World!\" on this line!"
+    );
+    assert_eq!(search_results.valid_todos[0].line_number, 4);
+    assert_eq!(
+        search_results.valid_todos[0].file,
+        Path::new("./tests/resources/base/file_with_todos.rs").to_path_buf()
+    );
+}
--- tests/resources/test-file.js
+++
@@ -1,6 +0,0 @@
-
-
-// @todo(2022-10-10): Refactor to typescript
-const msg = "Hello World!"
-console.log(msg);
-
