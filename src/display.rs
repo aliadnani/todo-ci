9982d48078704d79e386614f97d225aedc0997e0
Ali Adnan <ali.adnani@hotmail.com>
2022-07-09 18:07:01 +0800

    refactor: clean up error handling

--- src/display.rs
+++ src/display.rs
@@ -2,77 +2,67 @@ use chrono::Utc;
 
 use crate::{
     cli::DisplayMode,
-    core::{Todo, TotalTodoSearchResult},
+    core::{Todo, TotalSearchResult},
 };
 
-pub struct Printer {
-    mode: DisplayMode,
+fn print_single(todo: &Todo, overdue: bool) {
+    if overdue {
+        bunt::println!(
+            "{$bold+red}TODO@ {$underline} {}:{}{/$} [overdue]{/$}",
+            &todo.file.as_path().display().to_string(),
+            &todo.line_number
+        )
+    } else {
+        bunt::println!(
+            "{$bold}TODO@{/$} {$underline} {}:{}{/$}",
+            &todo.file.as_path().display().to_string(),
+            &todo.line_number
+        )
+    }
+
+    let date_str = &todo.date.format("%Y-%m-%d").to_string();
+    let day_difference = -Utc::now()
+        .date()
+        .naive_utc()
+        .signed_duration_since(todo.date)
+        .num_days();
+
+    bunt::println!("  {$red}Due:{/$} {} ({} days)", date_str, day_difference);
+    bunt::println!("  {$cyan}Description:{/$} {}", &todo.description);
+
+    bunt::println!();
 }
 
-impl Printer {
-    pub fn new(display_mode: DisplayMode) -> Printer {
-        Printer { mode: display_mode }
-    }
+pub fn print(mode: DisplayMode, results: &TotalSearchResult) {
 
-    fn print_single(&self, todo: &Todo, overdue: bool) {
-        if overdue {
-            bunt::println!(
-                "{$bold+red}TODO@ {$underline} {}:{}{/$} [overdue]{/$}",
-                &todo.file.as_path().display().to_string(),
-                &todo.line_number
-            )
-        } else {
-            bunt::println!(
-                "{$bold}TODO@{/$} {$underline} {}:{}{/$}",
-                &todo.file.as_path().display().to_string(),
-                &todo.line_number
-            )
-        }
+    // Total stats
+    bunt::println!("{$blue}Searched {} file(s):{/$}", results.files_searched);
+    bunt::print!(
+        "{$bold}{} todo(s) found{/$}",
+        results.valid_todos.len() + results.overdue_todos.len()
+    );
 
-        let date_str = &todo.date.format("%Y-%m-%d").to_string();
-        let day_difference = -Utc::now()
-            .date()
-            .naive_utc()
-            .signed_duration_since(todo.date)
-            .num_days();
-
-        bunt::println!("  {$red}Due:{/$} {} ({} days)", date_str, day_difference);
-        bunt::println!("  {$cyan}Description:{/$} {}", &todo.description);
-
-        bunt::println!();
-    }
-
-    pub fn print(&self, results: &TotalTodoSearchResult) {
-        // TODO: Tidy up the `println()`s
-        bunt::println!();
-        bunt::println!("{$blue}Searched {} file(s):{/$}", results.files_searched);
-        bunt::print!(
-            "{$bold}{} todo(s) found{/$}",
-            results.valid_todos.len() + results.overdue_todos.len()
+    if !results.overdue_todos.is_empty() {
+        bunt::println!(
+            "{$bold+red} of which {} is/are overdue{/$}",
+            results.overdue_todos.len(),
         );
-
-        if !results.overdue_todos.is_empty() {
-            bunt::println!(
-                "{$bold+red} of which {} is/are overdue{/$}",
-                results.overdue_todos.len(),
-            );
-        } else {
-            bunt::println!();
-        }
-
+    } else {
         bunt::println!();
+    }
 
-        if !matches!(self.mode, DisplayMode::Concise) {
-            if !matches!(self.mode, DisplayMode::OverdueOnly) {
-                results
-                    .valid_todos
-                    .iter()
-                    .for_each(|todo| self.print_single(todo, false));
-            }
+    bunt::println!();
+
+    if !matches!(mode, DisplayMode::Concise) {
+        if !matches!(mode, DisplayMode::OverdueOnly) {
             results
-                .overdue_todos
+                .valid_todos
                 .iter()
-                .for_each(|todo| self.print_single(todo, true));
+                .for_each(|todo| print_single(todo, false));
         }
+        results
+            .overdue_todos
+            .iter()
+            .for_each(|todo| print_single(todo, true));
     }
 }
