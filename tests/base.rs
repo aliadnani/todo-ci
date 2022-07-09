4b7894f046e54b9b449109ebbaf1f119e78818e9
Ali Adnan <ali.adnani@hotmail.com>
2022-07-09 18:18:22 +0800

    refactor: refactor for tests

---
+++ tests/base.rs
@@ -0,0 +1,12 @@
+// use std::path::{Path, PathBuf};
+
+// #[test]
+// fn test_add() {
+//     let search_results = todo_ci::core::search(
+//         Path::new("./resources/base").to_path_buf(),
+//         false,
+//         "*".to_string(),
+//     )
+//     .unwrap();
+//     // assert_eq!(display::search(3, 2), 5);
+// }
