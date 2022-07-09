5aa2d5d2719b305322ce6214f4151764706a3d3e
Ali Adnan <ali.adnani@hotmail.com>
2022-07-09 18:18:22 +0800

    Refactor for tests

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+pub mod cli;
+pub mod core;
+pub mod display;
+
+use clap::Parser;
+use eyre::Result;
+
+pub fn run() -> Result<()>{
+    // Get CLI args
+    let args = cli::Args::parse();
+
+    // Run todo search
+    let search_results = core::search(args.root_directory, args.no_ignore, args.ignore_pattern)?;
+
+    // Print results of search
+    display::print(args.display_mode, &search_results);
+
+    if !search_results.overdue_todos.is_empty() && !args.no_error {
+        std::process::exit(1)
+    };
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,4 @@
-mod cli;
-mod core;
-mod display;
-
-use clap::Parser;
-use eyre::Result;
-
-fn main() -> Result<()>{
-    // Get CLI args
-    let args = cli::Args::parse();
-
-    // Run todo search
-    let search_results = core::search(args.root_directory, args.no_ignore, args.ignore_pattern)?;
-
-    // Print results of search
-    display::print(args.display_mode, &search_results);
-
-    if !search_results.overdue_todos.is_empty() && !args.no_error {
-        std::process::exit(1)
-    };
-
+fn main() -> eyre::Result<()> {
+    todo_ci::run()?;
     Ok(())
 }
