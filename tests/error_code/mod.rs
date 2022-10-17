e1f02680ee12aef0ea462043c42ec0c0735305d7
Ali <ali.adnani@hotmail.com>
2022-10-17 23:31:03 +0800

    fix: fixed error code tests

--- tests/error_code/mod.rs
+++ tests/error_code/mod.rs
@@ -3,12 +3,23 @@ use assert_cmd::Command;
 #[test]
 fn test_error_code_emitted_if_overdue_todos_found() {
     let mut cmd = Command::cargo_bin("todo-ci").unwrap();
-    cmd.assert().failure();
+    cmd.current_dir("./tests/error_code/fixtures")
+        .assert()
+        .stdout(predicates::str::contains("error_code_file_with_todos.rs:3"))
+        .stdout(predicates::str::contains("error_code_file_with_todos.rs:4"))
+        .stdout(predicates::str::contains("2 todo(s) found"))
+        .failure();
 }
 
 #[test]
 fn test_error_code_can_be_supressed() {
     let mut cmd = Command::cargo_bin("todo-ci").unwrap();
-
-    cmd.args(["--no-error"]).assert().success();
+    
+    cmd.args(["--no-error"])
+        .current_dir("./tests/error_code/fixtures")
+        .assert()
+        .stdout(predicates::str::contains("error_code_file_with_todos.rs:3"))
+        .stdout(predicates::str::contains("error_code_file_with_todos.rs:4"))
+        .stdout(predicates::str::contains("2 todo(s) found"))
+        .success();
 }
